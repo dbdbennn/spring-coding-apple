e517607a697b387409987ecf665f9aebd8512be0
Yu Jung Eun <0hh.0553@gmail.com>
2025-04-10 12:49:21 +0900

    start

---
+++ src/main/java/com/apple/shop/BasicController.java
@@ -0,0 +1,21 @@
+package com.apple.shop;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.time.ZonedDateTime;
+
+@Controller
+public class BasicController {
+    @GetMapping("/")
+    String hello() {
+        return "index.html";
+    }
+
+    @GetMapping("/date")
+    @ResponseBody
+    String mypage() {
+        return ZonedDateTime.now().toString();
+    }
+}
---
+++ src/main/java/com/apple/shop/ItemController.java
@@ -0,0 +1,8 @@
+package com.apple.shop;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ItemController {
+    
+}
