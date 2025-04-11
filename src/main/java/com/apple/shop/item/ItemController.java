23c111a4191c2caaac17c60816337e6dde562fa8
Yu Jung Eun <0hh.0553@gmail.com>
2025-04-11 16:46:27 +0900

    Part 2:상세페이지 만들기 2 & 예외처리

--- src/main/java/com/apple/shop/item/ItemController.java
+++ src/main/java/com/apple/shop/item/ItemController.java
@@ -3,9 +3,10 @@ package com.apple.shop.item;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
@@ -19,5 +20,28 @@ public class ItemController {
         model.addAttribute("items", result);
 
         return "list.html";
+    }
+
+    @GetMapping("/write")
+    String write() {
+        return "write.html";
+    }
+
+    @PostMapping("/add")
+    String addItem(@ModelAttribute Item item) {
+        itemRepository.save(item);
+        return "redirect:/list";
+    }
+
+    @GetMapping("/detail/{id}")
+    String detail(@PathVariable Long id, Model model) {
+       Optional<Item> result =  itemRepository.findById(id);
+       // 데이터 있는지 체크 !sEmpty()
+       if(result.isPresent()) {
+           model.addAttribute("item", result.get());
+           return "detail.html";
+       } else {
+           return "redirect:/list";
+       }
     }
 }
